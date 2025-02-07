ab68a04fce050f9859c0aa5ca5909fe0b1e7feb3
XTQ <xtq@newbit.jp>
2025-02-07 18:31:10 +0900

    优化依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,30 @@
 plugins {
     id("java")
-    id("org.beryx.jlink") version "3.1.1"
-    id("com.bmuschko.docker-java-application") version "9.4.0"
+    id("org.beryx.jlink") version "3.1.+"
+    id("com.bmuschko.docker-java-application") version "9.4.+"
 }
 
 group = "com.nbintelligence"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:5.11.4"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
-    implementation("io.projectreactor.tools:blockhound:1.0.10.RELEASE")
+    implementation(platform("io.netty:netty-bom:4.1.117.Final"))
+    implementation("io.netty:netty-codec-http")
+    implementation("io.netty:netty-transport-native-epoll")
+    implementation("io.netty:netty-transport-native-kqueue")
 
-    implementation("io.netty:netty-transport-native-epoll:4.1.117.Final:linux-x86_64")
-    implementation("io.netty:netty-transport-native-epoll:4.1.117.Final:linux-aarch_64")
-    implementation("io.netty:netty-transport-native-kqueue:4.1.117.Final:osx-x86_64")
-    implementation("io.netty:netty-transport-native-kqueue:4.1.117.Final:osx-aarch_64")
-    implementation("io.netty:netty-codec-http:4.1.117.Final")
+    runtimeOnly("io.netty:netty-transport-native-epoll:4.1.117.Final:linux-x86_64")
+    runtimeOnly("io.netty:netty-transport-native-epoll:4.1.117.Final:linux-aarch_64")
+    runtimeOnly("io.netty:netty-transport-native-kqueue:4.1.117.Final:osx-x86_64")
+    runtimeOnly("io.netty:netty-transport-native-kqueue:4.1.117.Final:osx-aarch_64")
+    runtimeOnly("io.projectreactor.tools:blockhound:1.0.10.RELEASE")
 }
 
 application {
@@ -33,12 +36,6 @@ jlink {
     options = listOf(
         "--no-header-files",
     )
-
-    launcher {
-        jvmArgs = listOf(
-            "-Xms128m",
-        )
-    }
 }
 
 tasks.test {
